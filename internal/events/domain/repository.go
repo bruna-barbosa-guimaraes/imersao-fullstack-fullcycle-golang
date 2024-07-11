155f5e291d9b6072fe8976b9b4ab7083dd9b1334
Bruna Guimarães <62905224+bruna-barbosa-guimaraes@users.noreply.github.com>
2024-07-11 14:20:14 -0300

    event repository completo

--- internal/events/domain/repository.go
+++ internal/events/domain/repository.go
@@ -2,10 +2,10 @@ package domain
 
 type EventRepository interface {
 	ListEvents() ([]Event, error)
-	FindEventById(eventID string) (*Event, error)
+	FindEventByID(eventID string) (*Event, error)
 	FindSpotsByEventID(eventID string) ([]*Spot, error)
 	FindSpotByName(eventID, spotName string) (*Spot, error)
-	// CreateEvent(event *Event) error
+	CreateEvent(event *Event) error
 	CreateSpot(spot *Spot) error
 	CreateTicket(ticket *Ticket) error
 	ReserveSpot(spotID, ticketID string) error
